311a3b8f55a2425b8cdc6db2bb519c03ba88b5e6
Viktor Kuzemichev <oll.kuz@yandex.ru>
2022-08-14 20:08:43 +0300

    Add TerminalNumericKeypadCellNumberConverter

--- src/organization/tictactoe/Launcher.java
+++ src/organization/tictactoe/Launcher.java
@@ -5,12 +5,12 @@
 package organization.tictactoe;
 
 import organization.tictactoe.component.*;
-import organization.tictactoe.component.keypad.DesktopNumericKeypadCellNumberConverter;
+import organization.tictactoe.component.keypad.*;
 
 public class Launcher {
 
     public static void main(final String[] args) {
-        final CellNumberConverter cellNumberConverter = new DesktopNumericKeypadCellNumberConverter();
+        final CellNumberConverter cellNumberConverter = new TerminalNumericKeypadCellNumberConverter();
         final Game game = new Game(
                 new DataPrinter(cellNumberConverter),
                 new ComputerMove(),
---
+++ src/organization/tictactoe/component/keypad/TerminalNumericKeypadCellNumberConverter.java
@@ -0,0 +1,32 @@
+package organization.tictactoe.component.keypad;/*
+ * License Lance
+ */
+
+import organization.tictactoe.component.CellNumberConverter;
+import organization.tictactoe.model.Cell;
+
+public class TerminalNumericKeypadCellNumberConverter implements CellNumberConverter {
+
+    private final char[][] mapping = {
+            {'1', '2', '3'},
+            {'4', '5', '6'},
+            {'7', '8', '9'}
+    };
+
+    @Override
+    public Cell toCell(final char number) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (mapping[i][j] == number) {
+                    return new Cell(i, j);
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public char toNumber(final Cell cell) {
+        return mapping[cell.getRow()][cell.getCol()];
+    }
+}
