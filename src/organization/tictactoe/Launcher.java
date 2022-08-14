7522fc507cb1484babd18f8fe0a6e8517bd2ffe7
Viktor Kuzemichev <oll.kuz@yandex.ru>
2022-08-14 19:56:21 +0300

    Add CellNumberConverter component

--- src/organization/tictactoe/Launcher.java
+++ src/organization/tictactoe/Launcher.java
@@ -9,10 +9,11 @@ import organization.tictactoe.component.*;
 public class Launcher {
 
     public static void main(final String[] args) {
+        final CellNumberConverter cellNumberConverter = new CellNumberConverter();
         final Game game = new Game(
-                new DataPrinter(),
+                new DataPrinter(cellNumberConverter),
                 new ComputerMove(),
-                new UserMove(),
+                new UserMove(cellNumberConverter),
                 new WinnerVerifier(),
                 new CellVerifier()
         );
